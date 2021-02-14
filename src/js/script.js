b2f3be6abb1ab79688815dd30a88b74bfb133a85
Cornholius <y.layshkin@gmail.com>
2021-02-14 22:20:35 +0300

    первая версия исправлений

--- src/js/script.js
+++ src/js/script.js
@@ -7,23 +7,7 @@ $(document).ready(function(){
         dots: true,
         dotsClass: 'dots-style',
         arrows: true,
-        variableWidth: true,
-        prevArrow: '<button type="button" class="slick-prev"><div class="btn_arrow_left"></div></button>',
-        nextArrow: '<button type="button" class="slick-next"><div class="btn_arrow_right"></div></button>',
-    });
-});
-
-$(document).ready(function(){
-    $('.slider1').slick({
-        infinite: true,
-        autoplay: false,
-        speed: 1200,
-        autoplaySpeed: 2000,
-        dots: true,
-        dotsClass: 'dots-style',
-        arrows: true,
-        slidesToShow: 3,
-        variableWidth: true,
+        variableWidth: false,
         prevArrow: '<button type="button" class="slick-prev"><div class="btn_arrow_left"></div></button>',
         nextArrow: '<button type="button" class="slick-next"><div class="btn_arrow_right"></div></button>',
     });
