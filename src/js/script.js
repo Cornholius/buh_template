12606a3845cfef64ce14e62105006b99a9110734
lyi <y.layshkin@gmail.com>
2021-03-15 17:51:35 +0300

    мобильное меню. допилить

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,36 @@
+const anchors = document.querySelectorAll('a.anchor')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.mobile_menu'),
+  menuItem = document.querySelectorAll('.mobile_menu__item'),
+  hamburger = document.querySelector('.mobile_button');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('mobile_button__active');
+      menu.classList.toggle('mobile_menu__active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('mobile_button__active');
+          menu.classList.toggle('mobile_menu__active');
+      })
+  })
+})
+
 $(document).ready(function(){
     $('.slider').slick({
         infinite: true,
